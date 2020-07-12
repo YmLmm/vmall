09b94ed3ab93f214c8fca89a7af639816450b54d
YmImm <Se7enYoung.wym@gmail.com>
2020-07-12 04:37:52 -0400

    vmall

---
+++ vmall-parent/vmall-eureka/src/main/java/com/mall/EurekaApplication.java
@@ -0,0 +1,29 @@
+package com.mall;
+
+/*
+ * @ClassName: EurekaApplication
+ * @Description:
+ * @Author: Se7en
+ * @Date: 2020/7/12 4:10
+ * @Version: 1.0
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    /**
+    * @Description: Load the startup class as the current configuration standard for springboot
+    * @Param: [args]
+    * @return: void
+    * @Author: Se7en
+    * @Date: 2020/7/12
+    */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
