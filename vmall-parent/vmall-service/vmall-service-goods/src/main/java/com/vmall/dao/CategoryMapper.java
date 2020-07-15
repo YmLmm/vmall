4d92f376fcde9dad9043cdb451fdb9e19ef41c56
YmImm <Se7enYoung.wym@gmail.com>
2020-07-15 04:50:57 -0400

    增加商品分类

---
+++ vmall-parent/vmall-service/vmall-service-goods/src/main/java/com/vmall/dao/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.vmall.dao;
+/*
+ * @ClassName: CategoryMapper
+ * @Description: Interface
+ * @Author: Se7en
+ * @Date: 2020/7/15 4:43
+ * @Version: 1.0
+ */
+
+import com.vmall.goods.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CategoryMapper extends Mapper<Category> {
+}
