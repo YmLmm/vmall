46078de42246eccf8d2bafea233d53067979f942
YmImm <Se7enYoung.wym@gmail.com>
2020-07-15 04:50:32 -0400

    增加相册管理

---
+++ vmall-parent/vmall-service/vmall-service-goods/src/main/java/com/vmall/dao/AlbumMapper.java
@@ -0,0 +1,14 @@
+package com.vmall.dao;
+/*
+ * @ClassName: AlbumMapper
+ * @Description: Interface
+ * @Author: Se7en
+ * @Date: 2020/7/15 3:40
+ * @Version: 1.0
+ */
+
+import com.vmall.goods.pojo.Album;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AlbumMapper extends Mapper<Album> {
+}
